b810c5fa7e359dbb0c2a15a82de42e534a725268
ggarc <vincas-suns.9z@icloud.com>
2026-03-05 19:50:59 -0700

    adjust page spacing for header

--- app/page.tsx
+++ app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
 
   return (
 
-    <main className="min-h-screen bg-gray-50 flex flex-col items-center p-10">
+    <main className="py-16 px-6 flex flex-col items-center">
 
       <h1 className="text-4xl font-bold mb-2 text-center">
         Mortgage Payment Calculator
